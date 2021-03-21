782765b5e012e9390a1b2ae4bcf93c27e63fb543
Ivan Kuhta <filmplex22342@gmail.com>
2021-03-21 22:36:48 +0200

    feat: add UI class and events for buttons (fullscreen, letters, notes)

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -38,3 +38,92 @@ const pianoKeys = [...piano.querySelectorAll(pianoKeySetting.selectorPianoKey)];
 const btns = [...document.querySelectorAll(btnsSetting.globalSelector)];
 const fullscreenBtn = document.querySelector(btnsSetting.btnFullscreen.selector);
 
+// Classes
+class UI {
+  addClassLetterKeys(addClass) {
+    pianoKeys.forEach((pianoKey) => {
+      UI.addClass(pianoKey, addClass);
+    });
+  }
+  clearClassLetterKeys(clearClass) {
+    pianoKeys.forEach((pianoKey) => {
+      UI.removeClass(pianoKey, clearClass);
+    });
+  }
+
+  getFullscreen() {
+    return (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    );
+  }
+  openFullscreen(element) {
+    if (element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    } else if (element.msFullscreenElement) {
+      element.msRequestFullScreen();
+    }
+  }
+  cancelFullscreen() {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (documentElement.msCancelFullScreen) {
+      document.msCancelFullScreen();
+    }
+  }
+
+  Fullscreen() {
+    if (!this.getFullscreen()) {
+      this.openFullscreen(document.documentElement);
+    } else {
+      this.cancelFullscreen();
+    }
+  }
+
+  clearActiveClassBtns() {
+    btns.forEach((btn) => UI.removeClass(btn, btnsSetting.globalActiveClass));
+    this.clearClassLetterKeys(pianoKeySetting.letterKeyClass);
+  }
+
+  addClickEventBtns() {
+    btns
+      .find((btn) => btn.classList.contains(btnsSetting.btnLetters.class))
+      .addEventListener("click", (e) => {
+        this.clearActiveClassBtns();
+        UI.addClass(e.target, btnsSetting.globalActiveClass);
+        this.addClassLetterKeys(pianoKeySetting.letterKeyClass);
+      });
+    btns
+      .find((btn) => btn.classList.contains(btnsSetting.btnNotes.class))
+      .addEventListener("click", (e) => {
+        this.clearActiveClassBtns();
+        UI.addClass(e.target, btnsSetting.globalActiveClass);
+      });
+    fullscreenBtn.addEventListener("click", () => {
+      this.Fullscreen();
+    });
+  }
+
+  static addClass(elem, classElement) {
+    elem.classList.add(classElement);
+  }
+  static removeClass(elem, classElement) {
+    elem.classList.remove(classElement);
+  }
+}
+
+// App
+window.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  ui.addClickEventBtns();
+});
